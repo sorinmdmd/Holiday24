887dc18f057f2653b335b63453b0ba30ec32e3a2
Sorin Otel <otelsorin@gmail.com>
2025-04-13 14:31:29 +0200

    Verification fixed

--- classes/DbAccess.inc.php
+++ classes/DbAccess.inc.php
@@ -1,37 +1,54 @@
 <?php
 
-    class DbAccess {
+class DbAccess
+{
 
-        public static function getMonths($link)
-        {
-            $query="Select id, name
+    public static function getMonths($link)
+    {
+        $query = "Select id, name
                     from month";
-            return DbFunctions::getHash($link, $query);
-        }
-        public static function getTravelbundles($link)
-        {
-            $query = "SELECT t.id, t.country, t.city, t.available_spaces, t.price, t.hotelid, t.img_path,
+        return DbFunctions::getHash($link, $query);
+    }
+    public static function getTravelbundles($link)
+    {
+        $query = "SELECT t.id, t.country, t.city, t.available_spaces, t.price, t.hotelid, t.img_path,
                             h.name as hotel_name, t.start_date, t.end_date
                       FROM travelbundle t
                       LEFT JOIN hotel h ON t.hotelid = h.id";
-            return DbFunctions::getRows($link, $query);
-        }
-        public static function getUserDetails($link) {
-            $query = "SELECT *
-                      FROM customer";
-            return DbFunctions::getRows($link, $query);
-        }
-
-        public static function getUserById($link, $userId) {
-            $query = "SELECT * FROM customer WHERE id = '" . mysqli_real_escape_string($link, $userId) . "'";
-            return DbFunctions::getRows($link, $query);
-        }
-
-        public static function verifyUser($link, $user_id) {
-            $query = "UPDATE users SET verified = 1 WHERE id = ?";
-            $stmt = $link->prepare($query);
-            $stmt->bind_param("i", $user_id);
-            $stmt->execute();
-            $stmt->close();
-        }
+        return DbFunctions::getRows($link, $query);
     }
+    public static function getUserDetails($link)
+    {
+        $query = "SELECT *
+                      FROM customer";
+        return DbFunctions::getRows($link, $query);
+    }
+
+    public static function getUserById($link, $userId)
+    {
+        $query = "SELECT * FROM customer WHERE id = '" . mysqli_real_escape_string($link, $userId) . "'";
+        return DbFunctions::getRows($link, $query);
+    }
+
+    public static function verifyUser($link, $userid)
+    {
+        // Use a placeholder in the query
+        $query = "UPDATE customer SET verified = 1 WHERE id = ?";
+    
+        // Prepare the statement
+        $stmt = $link->prepare($query);
+    
+        if ($stmt) {
+            // Bind the parameter to the placeholder as a string
+            $stmt->bind_param("s", $userid); // Use "s" for string
+    
+            // Execute the statement
+            $stmt->execute();
+    
+            // Close the statement
+            $stmt->close();
+        } else {
+            error_log("Error preparing statement: " . $link->error);
+        }
+    }    
+}
