8feeabee4b0da8d69eecbb5a038def61b99a1a05
Dennis' MAC <dennismac@MacBook-Pro-von-ICCONSULT.local>
2025-04-15 12:44:45 +0200

    Adminpanel Anpassung v4

--- smarty/templates_c/fded5bbe70a523484021f76284ffd58f39b7060a_0.file.adminPanel.tpl.php
+++ smarty/templates_c/fded5bbe70a523484021f76284ffd58f39b7060a_0.file.adminPanel.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 4.2.0, created on 2025-04-15 10:15:17
+/* Smarty version 4.2.0, created on 2025-04-15 10:31:37
   from '/Users/dennismac/Documents/Projects/iksy2mainRep/iksy2/smarty/templates/adminPanel.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '4.2.0',
-  'unifunc' => 'content_67fe31b5cbb4b5_40623657',
+  'unifunc' => 'content_67fe3589a9f9b3_69497479',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     'fded5bbe70a523484021f76284ffd58f39b7060a' => 
     array (
       0 => '/Users/dennismac/Documents/Projects/iksy2mainRep/iksy2/smarty/templates/adminPanel.tpl',
-      1 => 1744712116,
+      1 => 1744713096,
       2 => 'file',
     ),
   ),
@@ -21,7 +21,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
     'file:header-admin.tpl' => 1,
   ),
 ),false)) {
-function content_67fe31b5cbb4b5_40623657 (Smarty_Internal_Template $_smarty_tpl) {
+function content_67fe3589a9f9b3_69497479 (Smarty_Internal_Template $_smarty_tpl) {
 $_smarty_tpl->_checkPlugins(array(0=>array('file'=>'/Users/dennismac/Documents/Projects/iksy2mainRep/iksy2/classes/smarty/libs/plugins/modifier.date_format.php','function'=>'smarty_modifier_date_format',),));
 ?>
 <!DOCTYPE html>
@@ -39,45 +39,47 @@ $_smarty_tpl->_checkPlugins(array(0=>array('file'=>'/Users/dennismac/Documents/P
     <?php $_smarty_tpl->_subTemplateRender("file:header-admin.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
 ?>
     <h1 id="adminH1">Admin Panel</h1>
-    <h1>User Management</h1>
-    <div id="userTable" class="benutzerTabelle">
-        <table>
-            <thead>
-                <tr>
-                    <th>First and Last Name</th>
-                    <th>Email</th>
-                    <th>Actions</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php
+    <div id="userTable">
+        <h1>User Management</h1>
+        <div class="benutzerTabelle">
+            <table>
+                <thead>
+                    <tr>
+                        <th>First and Last Name</th>
+                        <th>Email</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
 $_from = $_smarty_tpl->smarty->ext->_foreach->init($_smarty_tpl, $_smarty_tpl->tpl_vars['users']->value, 'user');
 $_smarty_tpl->tpl_vars['user']->do_else = true;
 if ($_from !== null) foreach ($_from as $_smarty_tpl->tpl_vars['user']->value) {
 $_smarty_tpl->tpl_vars['user']->do_else = false;
 ?>
-                    <tr>
-                        <td><?php echo $_smarty_tpl->tpl_vars['user']->value['first_name'];?>
+                        <tr>
+                            <td><?php echo $_smarty_tpl->tpl_vars['user']->value['first_name'];?>
  <?php echo $_smarty_tpl->tpl_vars['user']->value['last_name'];?>
 </td>
-                        <td><?php echo $_smarty_tpl->tpl_vars['user']->value['email'];?>
+                            <td><?php echo $_smarty_tpl->tpl_vars['user']->value['email'];?>
 </td>
-                        <td>
-                            <?php if ($_smarty_tpl->tpl_vars['user']->value['id'] == 0) {?>
-                            <?php } else { ?>
-                                <form method="POST" action="">
-                                    <input type="hidden" name="delete_user_id" value="<?php echo $_smarty_tpl->tpl_vars['user']->value['id'];?>
+                            <td>
+                                <?php if ($_smarty_tpl->tpl_vars['user']->value['id'] == 0) {?>
+                                <?php } else { ?>
+                                    <form method="POST" action="">
+                                        <input type="hidden" name="delete_user_id" value="<?php echo $_smarty_tpl->tpl_vars['user']->value['id'];?>
 ">
-                                    <button type="submit">Delete</button>
-                                </form>
-                            <?php }?>
-                        </td>
-                    </tr>
-                <?php
+                                        <button type="submit">Delete</button>
+                                    </form>
+                                <?php }?>
+                            </td>
+                        </tr>
+                    <?php
 }
 $_smarty_tpl->smarty->ext->_foreach->restore($_smarty_tpl, 1);?>
-            </tbody>
-        </table>
+                </tbody>
+            </table>
+        </div>
     </div>
     <div id="travelManagement">
             <h1>Travel Management</h1>
