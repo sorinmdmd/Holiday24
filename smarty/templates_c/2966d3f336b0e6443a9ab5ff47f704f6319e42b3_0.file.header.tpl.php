66bd48f130f033a90e963b09ff92b2a50b3a025e
AnaLuisaKelm <ana.schwengber-kelm@stud.hs-bochum.de>
2025-05-22 11:29:11 +0200

    Buchung und Stornierung implementiert

---
+++ smarty/templates_c/2966d3f336b0e6443a9ab5ff47f704f6319e42b3_0.file.header.tpl.php
@@ -0,0 +1,59 @@
+<?php
+/* Smarty version 4.2.0, created on 2025-05-22 09:24:33
+  from 'C:\Users\Hugo\OneDrive\HS Bochum\SS25\IKSY II\git - Kopie\iksy2\smarty\templates\header.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '4.2.0',
+  'unifunc' => 'content_682eed51c192a9_04342960',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '2966d3f336b0e6443a9ab5ff47f704f6319e42b3' => 
+    array (
+      0 => 'C:\\Users\\Hugo\\OneDrive\\HS Bochum\\SS25\\IKSY II\\git - Kopie\\iksy2\\smarty\\templates\\header.tpl',
+      1 => 1744976105,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_682eed51c192a9_04342960 (Smarty_Internal_Template $_smarty_tpl) {
+?><link rel="stylesheet" type="text/css" href="css/header.css">
+<link rel="stylesheet" type="text/css" href="css/global.css">
+<header>
+<button id="open-sidebar-button" onclick="openSidebar()">
+    <img src="images/menu-button.svg" height="40px" width="40px" fill="#FFA725">
+</button>  
+<nav id="navbar">
+    <ul>
+        <li>
+            <button id="close-sidebar-button" onclick="closeSidebar()">
+                <img src="images/close-button.svg" height="40px" width="40px" fill="#FFA725">
+            </button>
+        </li>
+        <li class="home-li"><a class="<?php if ($_smarty_tpl->tpl_vars['activePage']->value == 'index') {?>active-link<?php }?>" href="index.php">Home</a></li>
+        <?php if ((isset($_smarty_tpl->tpl_vars['user_id']->value)) && $_smarty_tpl->tpl_vars['user_role']->value == 'customer') {?>
+            <li><a class="<?php if ($_smarty_tpl->tpl_vars['activePage']->value == 'mytravelpacks') {?>active-link<?php }?>" href="mytravelpacks.php">My Travel Packs</a></li>
+            <li><a class="<?php if ($_smarty_tpl->tpl_vars['activePage']->value == 'myProfile') {?>active-link<?php }?>" href="myProfile.php">My Profile</a></li>
+        <?php }?>
+        <li><a class="<?php if ($_smarty_tpl->tpl_vars['activePage']->value == 'ouroffers') {?>active-link<?php }?>" href="ouroffers.php">Our trips</a></li>
+        <li><a class="<?php if ($_smarty_tpl->tpl_vars['activePage']->value == 'aboutus') {?>active-link<?php }?>" href="index.php#aboutusId">About us</a></li>
+        <li>
+            <?php if ((isset($_smarty_tpl->tpl_vars['user_id']->value))) {?>
+                <a class="accent-link" href="logout.php">Logout</a>
+            <?php } else { ?>
+                <a class="accent-link" href="login.php">Login</a>
+            <?php }?>
+        </li>
+    </ul>
+</nav>
+<div id="overlay" onclick="closeSidebar()"></div>      
+<?php echo '<script'; ?>
+ src="script.js" defer><?php echo '</script'; ?>
+>
+</header>
+<?php }
+}
---
+++ smarty/templates_c/7069fce3125d184dd7cefc96f37308675104d5b3_0.file.aboutus.tpl.php
@@ -0,0 +1,48 @@
+<?php
+/* Smarty version 4.2.0, created on 2025-05-14 08:36:57
+  from 'C:\Users\Acer\OneDrive\HS Bochum\SS25\IKSY II\git - Kopie\iksy2\smarty\templates\aboutus.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '4.2.0',
+  'unifunc' => 'content_68245629ebd7a4_24883916',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '7069fce3125d184dd7cefc96f37308675104d5b3' => 
+    array (
+      0 => 'C:\\Users\\Acer\\OneDrive\\HS Bochum\\SS25\\IKSY II\\git - Kopie\\iksy2\\smarty\\templates\\aboutus.tpl',
+      1 => 1744975989,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_68245629ebd7a4_24883916 (Smarty_Internal_Template $_smarty_tpl) {
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title><?php echo $_smarty_tpl->tpl_vars['title']->value;?>
+</title>
+    <link rel="stylesheet" type="text/css" href="css/global.css">
+    <link rel="stylesheet" type="text/css" href="css/aboutus.css">
+    <link rel="icon" href="images/logo.png" type="image/png">
+</head>
+<body>
+    <main>
+        <h1><?php echo $_smarty_tpl->tpl_vars['title']->value;?>
+</h1>
+        <section id="about-us">
+            <p>Welcome to Holiday24, your ultimate destination for hassle-free travel planning. We specialize in curating exceptional travel bundles that include top-notch hotels and exciting experiences, all tailored to make your journey unforgettable.</p>
+            <p>Our mission is to provide you with seamless and affordable travel solutions. Whether you're planning a romantic getaway, a family vacation, or an adventure-packed trip, our carefully crafted bundles ensure that every aspect of your travel is covered.</p>
+            <p>With a passion for travel and a commitment to excellence, our team works tirelessly to handpick the best hotels and local attractions. We believe that travel should be enjoyable and stress-free, and we strive to make that a reality for each of our customers.</p>
+            <p>Join us on this journey and let us help you create memories that will last a lifetime. Your adventure awaits!</p>
+        </section>
+    </main>
+</body>
+</html>
+<?php }
+}
