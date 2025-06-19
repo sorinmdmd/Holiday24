6c360f1f37acb92be681cd6fc8771a3e4396fede
Sorin Otel <otelsorin@gmail.com>
2025-06-19 11:59:05 +0200

    Added possibility to add travel bund

--- smarty/templates/headerAdmin.tpl
+++ smarty/templates/headerAdmin.tpl
@@ -11,11 +11,11 @@
             </button>
         </li>
         <li>
-            <li><a class="{if $activePage == 'adminPanel'}active-link{/if}" href="adminPanel.php">ADMIN</a></li>
+<li><a class="{if isset($activePage) and $activePage == 'adminPanel'}active-link{/if}" href="adminPanel.php">ADMIN</a></li>
             <li><a href="adminPanel.php#userTable">Manage Users</a></li>
             <li><a href="adminPanel.php#travelManagement">Manage Travel</a></li>
-            <li><a class="{if $activePage == 'ouroffers'}active-link{/if}" href="ourOffers.php">Travelpacks</a></li> 
-            <li><a class="accent-link" href="logout.php">Logout - {$user_role}</a></li>
+<li><a class="{if isset($activePage) and $activePage == 'ouroffers'}active-link{/if}" href="ourOffers.php">Travelpacks</a></li>
+<li><a class="accent-link" href="logout.php">Logout{if isset($user_role)} - {$user_role}{/if}</a></li>
         </li>
     </ul>
 </nav>
--- smarty/templates_c/c27c71f76f192b02460e0777c0d239cdc9debf5a_0.file.headerAdmin.tpl.php
+++ smarty/templates_c/c27c71f76f192b02460e0777c0d239cdc9debf5a_0.file.headerAdmin.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 4.2.0, created on 2025-06-03 09:00:18
+/* Smarty version 4.2.0, created on 2025-06-19 09:46:17
   from '/Users/sorinotel/Documents/iksy2/Holiday24/smarty/templates/headerAdmin.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '4.2.0',
-  'unifunc' => 'content_683eb9a28f7459_33389457',
+  'unifunc' => 'content_6853dc690e7c18_50452895',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     'c27c71f76f192b02460e0777c0d239cdc9debf5a' => 
     array (
       0 => '/Users/sorinotel/Documents/iksy2/Holiday24/smarty/templates/headerAdmin.tpl',
-      1 => 1748940002,
+      1 => 1750326375,
       2 => 'file',
     ),
   ),
@@ -20,7 +20,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_683eb9a28f7459_33389457 (Smarty_Internal_Template $_smarty_tpl) {
+function content_6853dc690e7c18_50452895 (Smarty_Internal_Template $_smarty_tpl) {
 ?><link rel="stylesheet" type="text/css" href="css/headerAdmin.css">
 <header>
 <button id="open-sidebar-button" onclick="openSidebar()">
@@ -34,12 +34,12 @@ function content_683eb9a28f7459_33389457 (Smarty_Internal_Template $_smarty_tpl)
             </button>
         </li>
         <li>
-            <li><a class="<?php if ($_smarty_tpl->tpl_vars['activePage']->value == 'adminPanel') {?>active-link<?php }?>" href="adminPanel.php">ADMIN</a></li>
+<li><a class="<?php if ((isset($_smarty_tpl->tpl_vars['activePage']->value)) && $_smarty_tpl->tpl_vars['activePage']->value == 'adminPanel') {?>active-link<?php }?>" href="adminPanel.php">ADMIN</a></li>
             <li><a href="adminPanel.php#userTable">Manage Users</a></li>
             <li><a href="adminPanel.php#travelManagement">Manage Travel</a></li>
-            <li><a class="<?php if ($_smarty_tpl->tpl_vars['activePage']->value == 'ouroffers') {?>active-link<?php }?>" href="ourOffers.php">Travelpacks</a></li> 
-            <li><a class="accent-link" href="logout.php">Logout - <?php echo $_smarty_tpl->tpl_vars['user_role']->value;?>
-</a></li>
+<li><a class="<?php if ((isset($_smarty_tpl->tpl_vars['activePage']->value)) && $_smarty_tpl->tpl_vars['activePage']->value == 'ouroffers') {?>active-link<?php }?>" href="ourOffers.php">Travelpacks</a></li>
+<li><a class="accent-link" href="logout.php">Logout<?php if ((isset($_smarty_tpl->tpl_vars['user_role']->value))) {?> - <?php echo $_smarty_tpl->tpl_vars['user_role']->value;
+}?></a></li>
         </li>
     </ul>
 </nav>
